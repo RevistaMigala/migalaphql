4b5644e13be7a6f47d3892883950a0be60854baa
David <david.enriquez@zx-ventures.mx>
2023-04-24 17:32:58 -0600

    Remove unused code

--- src/handlers/alive/index.js
+++
@@ -1,9 +0,0 @@
-export const aliveResolver = async (root, args, context, info) => {
-    console.log('Query', info.fieldName)
-    console.log('Input', args)
-
-    return {
-        statusCode: 200,
-        message: 'Its aliveeeee!',
-    }
-}
--- src/resolvers/products.ts
+++ src/resolvers/products.ts
@@ -1,19 +1,6 @@
 import models from "../models";
 import connectDB from "../config/db";
 
-// const products = [
-//   {
-//     id: "1",
-//     title: "Migala 1.1",
-//     price: 500,
-//   },
-//   {
-//     id: "2",
-//     title: "CICATRICES",
-//     price: 200,
-//   },
-// ];
-
 const resolver = async () => {
   connectDB({ dbName: "love-e-commerce-dev" });
   return await models.Product.find();
